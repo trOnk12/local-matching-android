9dc0ad0dbbd2e3a9176907ba7d3efba05c6dd80b
trOnk125 <68686867+trOnk125@users.noreply.github.com>
2020-10-11 21:23:01 +0200

    require permission from library end-user

--- app/src/main/java/com/example/local_matching/worker/LocationWorker.kt
+++ app/src/main/java/com/example/local_matching/worker/LocationWorker.kt
@@ -1,15 +1,19 @@
 package com.example.local_matching.worker
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
 import android.util.Log
+import androidx.core.app.ActivityCompat
 import androidx.work.*
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import kotlinx.coroutines.suspendCancellableCoroutine
 import java.util.concurrent.TimeUnit
 import kotlin.coroutines.resume
+import kotlin.coroutines.resumeWithException
 
 class LocationWorker(appContext: Context, workersParams: WorkerParameters) :
     CoroutineWorker(appContext, workersParams) {
@@ -17,7 +21,9 @@ class LocationWorker(appContext: Context, workersParams: WorkerParameters) :
     private val locationRetriever = LocationRetriever(appContext)
 
     override suspend fun doWork(): Result {
-        Log.d("TEST", "location is ${locationRetriever.getUserLocation()}")
+    //    locationRetriever.getUserLocation()
+
+        Log.d("TEST", "location is ")
 
         return Result.success()
     }
@@ -27,15 +33,15 @@ class LocationWorker(appContext: Context, workersParams: WorkerParameters) :
 class LocationWorkerManager(private val appContext: Context) {
     companion object {
         private const val LOCATION_PERIODIC_WORK_NAME = "location_periodic_work"
-        private const val MINIMUM_PERIODIC_TIME_INTERVAL: Long = 15
+        private const val MINIMUM_PERIODIC_TIME_INTERVAL_IN_MINUTES: Long = 15
     }
 
     fun startPeriodRequest(
         requestInterval: RequestInterval = RequestInterval(
-            MINIMUM_PERIODIC_TIME_INTERVAL
+            MINIMUM_PERIODIC_TIME_INTERVAL_IN_MINUTES
         )
     ) {
-        if (requestInterval.minutes < MINIMUM_PERIODIC_TIME_INTERVAL) {
+        if (requestInterval.minutes < MINIMUM_PERIODIC_TIME_INTERVAL_IN_MINUTES) {
             enqueuePeriodicWork(buildPeriodicWorkRequest(requestInterval))
         } else {
             enqueuePeriodicWork(buildPeriodicWorkRequest(requestInterval))
@@ -58,18 +64,37 @@ class LocationWorkerManager(private val appContext: Context) {
 
 }
 
-class LocationRetriever(appContext: Context) {
+class LocationRetriever(private val appContext: Context) {
 
     private val fusedLocationClient: FusedLocationProviderClient =
         LocationServices.getFusedLocationProviderClient(appContext)
 
-    @SuppressLint("MissingPermission")
-    suspend fun getUserLocation() = suspendCancellableCoroutine<Location> { continuation ->
-        fusedLocationClient.lastLocation
-            .addOnSuccessListener { location: Location? ->
-                continuation.resume(location!!)
-            }
+    suspend fun getUserLocation(): Location {
+        checkRequiredPermissions()
+
+        return getLocation()
     }
+
+    private fun checkRequiredPermissions() {
+        if (ActivityCompat.checkSelfPermission(
+                appContext,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                appContext,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            throw Exception("This library required ACCESS_FINE_LOCATION and ACCESS_COARSE_LOCATION")
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private suspend fun getLocation() =
+        suspendCancellableCoroutine<Location> { cancellableContinuation ->
+            fusedLocationClient.lastLocation.addOnSuccessListener { location ->
+                cancellableContinuation.resume(location)
+            }
+        }
 
 }
 
