6e8a9e0a1b036c8984fb39ead333f8529728184e
trOnk125 <68686867+trOnk125@users.noreply.github.com>
2020-10-11 22:05:56 +0200

    add initilazer to start requesting location periodically

--- app/src/main/java/com/example/local_matching/MainActivity.kt
+++ app/src/main/java/com/example/local_matching/MainActivity.kt
@@ -2,19 +2,12 @@ package com.example.local_matching
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.local_matching.worker.LocationWorkerManager
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-        val locationWorker = LocationWorkerManager(this)
-
-        locationWorker.startPeriodRequest()
-
 
     }
 
---
+++ app/src/main/java/com/example/local_matching/initializer/WorkerMangerInitializer.kt
@@ -0,0 +1,66 @@
+package com.example.local_matching.initializer
+
+import android.content.ContentProvider
+import android.content.ContentValues
+import android.content.Context
+import android.content.pm.ProviderInfo
+import android.database.Cursor
+import android.net.Uri
+import com.example.local_matching.worker.LocationWorkerManager
+
+class WorkerMangerInitializer : ContentProvider() {
+
+    private val locationWorkerManager : LocationWorkerManager by lazy{
+         LocationWorkerManager(requireContext())
+    }
+
+    override fun onCreate(): Boolean {
+        locationWorkerManager.startPeriodRequest()
+
+        return true
+    }
+
+    override fun query(
+        p0: Uri,
+        p1: Array<out String>?,
+        p2: String?,
+        p3: Array<out String>?,
+        p4: String?
+    ): Cursor? {
+        return null
+    }
+
+    override fun getType(p0: Uri): String? {
+        return null
+    }
+
+    override fun insert(p0: Uri, p1: ContentValues?): Uri? {
+        return null
+    }
+
+    override fun delete(p0: Uri, p1: String?, p2: Array<out String>?): Int {
+        return 0
+    }
+
+    override fun update(p0: Uri, p1: ContentValues?, p2: String?, p3: Array<out String>?): Int {
+        return 0
+    }
+
+    override fun attachInfo(context: Context?, info: ProviderInfo?) {
+        checkIfProvidedInfoIsCorrect(info)
+
+        super.attachInfo(context, info)
+    }
+
+    private fun checkIfProvidedInfoIsCorrect(info: ProviderInfo?) {
+        if (info == null) {
+            throw NullPointerException("YourLibraryInitProvider ProviderInfo cannot be null.");
+        }
+        // So if the authorities equal the library internal ones, the developer forgot to set his applicationId
+        if ("com.example.local_matching.yourlibraryinitprovider" == info.authority) {
+            throw IllegalStateException("Incorrect provider authority in manifest. Most likely due to a "
+                    + "missing applicationId variable in application\'s build.gradle.")
+        }
+    }
+
+}
